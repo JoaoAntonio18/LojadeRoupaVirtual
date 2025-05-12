4e7e333ad19d95b061cb5b15531d0cea2c246c4e
AllmightFRC <joaoantoniosantos1018@gmail.com>
2025-05-12 08:37:26 -0300

    Criando o card dos produtos

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from '../components/card';
 
 const Products = () => {
   return (
     <section className="products">
       <h2>Todos os Produtos</h2>
       <p>Catálogo completo de roupas disponíveis.</p>
+      <Card />
     </section>
   );
 };
